aa4a8ee6c3854b45d9b45225ad7500afbdd4a0d0
mayo2451 <110712721+mayo2451@users.noreply.github.com>
2025-07-06 10:08:03 +0900

    Update app.py

--- 01miyakeiba - v1.1/Miyakeiba_app/app.py
+++ 01miyakeiba - v1.1/Miyakeiba_app/app.py
@@ -2,7 +2,6 @@ from flask import Flask,render_template,request,redirect, session, url_for, flas
 from flask_login import LoginManager, login_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
 import calendar
-import datetime
 from calendar import monthrange
 import jpholiday # type: ignore
 import sqlite3
