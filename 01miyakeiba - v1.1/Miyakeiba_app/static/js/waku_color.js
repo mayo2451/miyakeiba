dd0d460009bcf0b48e6821dd98891347b119b4d0
mayo2451 <110712721+mayo2451@users.noreply.github.com>
2025-08-02 01:13:30 +0900

    Update waku_color.js

--- 01miyakeiba - v1.1/Miyakeiba_app/static/js/waku_color.js
+++ 01miyakeiba - v1.1/Miyakeiba_app/static/js/waku_color.js
@@ -3,7 +3,7 @@ document.querySelectorAll(".uma").forEach(row => {
 	const total = parseInt(row.dataset.total);
 	let colorClass = "";
 
-	if (total = 18) {
+	if (total === 18) {
 		if ([1,2].includes(num)) colorClass = "waku-1";
 		if ([3,4].includes(num)) colorClass = "waku-2";
 		if ([5,6].includes(num)) colorClass = "waku-3";
@@ -12,7 +12,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([11,12].includes(num)) colorClass = "waku-6";
 		if ([13,14,15].includes(num)) colorClass = "waku-7";
 		if ([16,17,18].includes(num)) colorClass = "waku-8";
-	} else if (total = 17) {
+	} else if (total === 17) {
 		if ([1,2].includes(num)) colorClass = "waku-1";
 		if ([3,4].includes(num)) colorClass = "waku-2";
 		if ([5,6].includes(num)) colorClass = "waku-3";
@@ -21,7 +21,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([11,12].includes(num)) colorClass = "waku-6";
 		if ([13,14].includes(num)) colorClass = "waku-7";
 		if ([15,16,17].includes(num)) colorClass = "waku-8";
-	} else if (total = 16) {
+	} else if (total === 16) {
 		if ([1,2].includes(num)) colorClass = "waku-1";
 		if ([3,4].includes(num)) colorClass = "waku-2";
 		if ([5,6].includes(num)) colorClass = "waku-3";
@@ -30,7 +30,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([11,12].includes(num)) colorClass = "waku-6";
 		if ([13,14].includes(num)) colorClass = "waku-7";
 		if ([15,16].includes(num)) colorClass = "waku-8";
-	} else if (total = 15) {
+	} else if (total === 15) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2,3].includes(num)) colorClass = "waku-2";
 		if ([4,5].includes(num)) colorClass = "waku-3";
@@ -39,7 +39,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([10,11].includes(num)) colorClass = "waku-6";
 		if ([12,13].includes(num)) colorClass = "waku-7";
 		if ([14,15].includes(num)) colorClass = "waku-8";
-	} else if (total = 14) {
+	} else if (total === 14) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3,4].includes(num)) colorClass = "waku-3";
@@ -48,7 +48,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([9,10].includes(num)) colorClass = "waku-6";
 		if ([11,12].includes(num)) colorClass = "waku-7";
 		if ([13,14].includes(num)) colorClass = "waku-8";
-	} else if (total = 13) {
+	} else if (total === 13) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3].includes(num)) colorClass = "waku-3";
@@ -57,7 +57,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([8,9].includes(num)) colorClass = "waku-6";
 		if ([10,11].includes(num)) colorClass = "waku-7";
 		if ([12,13].includes(num)) colorClass = "waku-8";
-	} else if (total = 12) {
+	} else if (total === 12) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3].includes(num)) colorClass = "waku-3";
@@ -66,7 +66,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([7,8].includes(num)) colorClass = "waku-6";
 		if ([9,10].includes(num)) colorClass = "waku-7";
 		if ([11,12].includes(num)) colorClass = "waku-8";
-	} else if (total = 11) {
+	} else if (total === 11) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3].includes(num)) colorClass = "waku-3";
@@ -75,7 +75,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([6,7].includes(num)) colorClass = "waku-6";
 		if ([8,9].includes(num)) colorClass = "waku-7";
 		if ([10,11].includes(num)) colorClass = "waku-8";
-	} else if (total = 10) {
+	} else if (total === 10) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3].includes(num)) colorClass = "waku-3";
@@ -84,7 +84,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([6].includes(num)) colorClass = "waku-6";
 		if ([7,8].includes(num)) colorClass = "waku-7";
 		if ([9,10].includes(num)) colorClass = "waku-8";
-	} else if (total = 9) {
+	} else if (total === 9) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3].includes(num)) colorClass = "waku-3";
@@ -93,7 +93,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([6].includes(num)) colorClass = "waku-6";
 		if ([7].includes(num)) colorClass = "waku-7";
 		if ([8,9].includes(num)) colorClass = "waku-8";
-	} else if (total = 8) {
+	} else if (total === 8) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3].includes(num)) colorClass = "waku-3";
@@ -102,7 +102,7 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([6].includes(num)) colorClass = "waku-6";
 		if ([7].includes(num)) colorClass = "waku-7";
 		if ([8].includes(num)) colorClass = "waku-8";
-	} else if (total = 7) {
+	} else if (total === 7) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3].includes(num)) colorClass = "waku-3";
@@ -110,14 +110,14 @@ document.querySelectorAll(".uma").forEach(row => {
 		if ([5].includes(num)) colorClass = "waku-5";
 		if ([6].includes(num)) colorClass = "waku-6";
 		if ([7].includes(num)) colorClass = "waku-7";
-	} else if (total = 6) {
+	} else if (total === 6) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3].includes(num)) colorClass = "waku-3";
 		if ([4].includes(num)) colorClass = "waku-4";
 		if ([5].includes(num)) colorClass = "waku-5";
 		if ([6].includes(num)) colorClass = "waku-6";
-	} else if (total = 5) {
+	} else if (total === 5) {
 		if ([1].includes(num)) colorClass = "waku-1";
 		if ([2].includes(num)) colorClass = "waku-2";
 		if ([3].includes(num)) colorClass = "waku-3";
